f977fc6be7cf241af9bdb56c2bc2adf7a9804b87
Chase Violet <ccviol01@gmail.com>
2018-03-21 00:01:37 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+# [START submitted]
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+    # [END submitted]
+    # [START render_template]
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
+    # [END render_template]
